eaad9218b520df7b02f7844cb4e13ee438805f96
jivansh tyagi <90756453+jaityagiji@users.noreply.github.com>
2023-12-01 14:29:34 +0530

    Update db.js

--- middleware/db.js
+++ middleware/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const uri = "mongodb+srv://jivansh_Tyagi:fuckyou007@atlascluster.0psd8as.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://<Username>:<Password>@atlascluster.0psd8as.mongodb.net/?retryWrites=true&w=majority";
 
 exports.connectToDb = () => {
     mongoose.connect(uri)
