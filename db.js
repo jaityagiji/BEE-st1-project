094b89d0e1843c660e95bd5fd8520ddf57f945d6
jivansh tyagi <90756453+jaityagiji@users.noreply.github.com>
2023-12-01 14:28:27 +0530

    Delete db.js

--- db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const uri = "mongodb+srv://jivansh_Tyagi:fuckyou007@atlascluster.0psd8as.mongodb.net/?retryWrites=true&w=majority";
-
-exports.connectToDb = () => {
-    mongoose.connect(uri)
-    .then(() => {(console.log("DB connected"))})
-    .catch((err) => {(console.log("Not connected", err))});
-}
